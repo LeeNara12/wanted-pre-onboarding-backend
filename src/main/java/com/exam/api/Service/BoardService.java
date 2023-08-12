02db7b7724b09a853c41ad21e073dc7230a2a493
Jonghai <ljh7859@naver.com>
2023-08-12 12:08:33 +0900

    feat: list 기능 추가 - thymeleaf 적용 - 부트스트랩 사용 - list 출력기능

--- src/main/java/com/exam/api/Service/BoardService.java
+++ src/main/java/com/exam/api/Service/BoardService.java
@@ -5,6 +5,8 @@ import com.exam.api.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -12,5 +14,9 @@ public class BoardService {
     private BoardRepository boardRepository;
     public void write(Board board){
         boardRepository.save(board);
+    }
+
+    public List<Board> list(){
+        return boardRepository.findAll();
     }
 }
--- src/main/java/com/exam/api/controller/BoardController.java
+++ src/main/java/com/exam/api/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.exam.api.Service.BoardService;
 import com.exam.api.entity.Board;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -13,16 +14,28 @@ public class BoardController {
     @Autowired //읽어와서 주입 DI(Defendency Injection) 외부에서 두 객체 간의 관계를 결정해주는 디자인 패턴
     private BoardService boardService;
 
-    @GetMapping("/board/write")
+    @GetMapping("/board/write") //게시글 작성페이지로 이동
     public String boardWriteForm(){
         return "boardWrite";
     }
 
-    @PostMapping("/board/writePro")
+    @PostMapping("/board/writePro") //게시글 db에 저장
     public String boardWritePro(Board board){
 
         boardService.write(board);
 
         return "";
     }
+
+    @GetMapping("board/list") //게시글 리스트 페이지로 이동
+    public String boardList(Model model){
+        model.addAttribute("list", boardService.list());
+
+        return "boardList";
+    }
+
+
+
+
+
 }
